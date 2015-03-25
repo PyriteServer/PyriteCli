399195c708972fc442ece34de57575ab4780f139
Stefan Gordon <stefan@keirgordon.com>
2015-03-25 16:07:51 -0700

    Persisting prototype work on texture handling

--- Cuber/Program.cs
+++ Cuber/Program.cs
@@ -27,6 +27,17 @@ namespace Cuber
 			{
 				opt = CliParser.Parse<Options>(args);
 
+				// Testing!!!
+				if (opt.DebugTest)
+				{
+					CubeManager manager = new CubeManager(opt.Input[0], opt.xSize, opt.ySize, opt.zSize);
+					Texture texture = new Texture(manager.ObjInstance);
+					texture.NaiveTriangleRepack(opt.Input[1], opt.OutputPath);
+					return;
+				}
+
+				// End Testing
+
 				foreach (string path in opt.Input)
 				{
 					// Check if we are processing an image or a mesh
@@ -103,6 +114,10 @@ namespace Cuber
 		[NamedArgument('e', "ebo", Action = ParseAction.StoreTrue,
 			Description = "Generate EBO files designed for use with CubeServer in addition to OBJ files")]
 		public bool Ebo { get; set; }
+
+		[NamedArgument('d', "debug", Action = ParseAction.StoreTrue,
+			Description = "flag for testing prototype features")]
+		public bool DebugTest { get; set; }
 
 		[NamedArgument('r', "resume", Action = ParseAction.StoreTrue,
 			Description = "Existing files are not overwritten, but instead skipped")]
---
+++ CuberLib/Texture.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CuberLib.Types;
+
+namespace CuberLib
+{
+	public class Texture
+	{
+		private Obj obj;
+		private Bitmap[,] texture;
+
+		public Texture(Obj obj)
+		{
+			this.obj = obj;
+		}
+
+		public void NaiveTriangleRepack(string texturePath, string outputPath)
+		{
+			
+			List<Face> chunkFaceList;
+			chunkFaceList = obj.FaceList.AsParallel().Where(v => v.InExtent(
+				new Extent()
+				{
+					XMax = obj.Size.XMax - (obj.Size.XSize / 2),
+					YMax = obj.Size.YMax - (obj.Size.YSize / 2),
+					ZMax = obj.Size.ZMax,
+					XMin = obj.Size.XMin,
+					YMin = obj.Size.YMin,
+					ZMin = obj.Size.ZMin
+				}, obj.VertexList)).ToList();
+
+			// get list of UV triangles (maybe not distinct?)
+			var triangles = chunkFaceList.Select(f => new Tuple<TextureVertex, TextureVertex, TextureVertex>(
+				obj.TextureList[f.TextureVertexIndexList[0] - 1],
+				obj.TextureList[f.TextureVertexIndexList[1] - 1],
+				obj.TextureList[f.TextureVertexIndexList[2] - 1])).ToList();
+
+			// Load original texture and initiate new texture
+			Image original = Image.FromFile(texturePath);
+			Image output = new Bitmap(original.Width, original.Height);
+			using (Graphics destGraphics = Graphics.FromImage(output))
+			{
+
+				// write into same location in new bitmap
+				for (int i = 0; i < triangles.Count; i++)
+				{
+					var triangle = triangles[i];
+					var poly = new PointF[] {
+					new PointF((float)(triangle.Item1.X * original.Width), (float)((1-triangle.Item1.Y) * original.Height)),
+					new PointF((float)(triangle.Item2.X * original.Width), (float)((1-triangle.Item2.Y) * original.Height)),
+					new PointF((float)(triangle.Item3.X * original.Width), (float)((1-triangle.Item3.Y) * original.Height)),
+					new PointF((float)(triangle.Item1.X * original.Width), (float)((1-triangle.Item1.Y) * original.Height))
+					};
+
+					CopyPolygon(original, destGraphics, poly, poly);
+
+					if (i % 10000 == 0)
+					{
+						Console.WriteLine("{0} of {1}", i, triangles.Count);
+					}
+				}
+			}
+
+			string path = Path.Combine(outputPath, "output.jpg");
+			if (File.Exists(path)) File.Delete(path);
+            output.Save(path, ImageFormat.Jpeg);
+
+			output.Dispose();
+			original.Dispose();
+		}
+
+		private void CopyPolygon(Image source, Graphics dest, PointF[] sourcePoly, PointF[] destPoly)
+		{
+			using (GraphicsPath gpdest = new GraphicsPath())
+			using (GraphicsPath gpdestWide = new GraphicsPath())
+			{
+				gpdest.AddPolygon(destPoly);
+				gpdestWide.AddPolygon(destPoly);
+				gpdestWide.Widen(Pens.Black);
+
+				//Draw on the Bitmap
+				dest.SetClip(gpdest);
+				dest.SetClip(gpdestWide, CombineMode.Union);
+				dest.DrawImage(source, 0, 0);
+			}
+		}
+	}
+}
