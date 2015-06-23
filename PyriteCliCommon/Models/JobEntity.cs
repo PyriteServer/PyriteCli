fd8bea4b709159e3e7d13620b24b37253a412185
Stefan Gordon <stefan@stefangordon.com>
2015-06-23 08:46:28 -0700

    Configuration updates

---
+++ PyriteCliCommon/Models/JobEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace PyriteCliCommon.Models
+{
+    public class JobEntity : TableEntity
+    {
+        public JobEntity(string MeshName, string jobID)
+        : base(MeshName, jobID) { }
+
+        public JobEntity() { }
+
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+    }
+}
