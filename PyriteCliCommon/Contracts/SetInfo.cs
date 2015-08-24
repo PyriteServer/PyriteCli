2d0578cdfce33f13b492984fa7982af8aaa3b485
Andre Price <obsoleted@users.noreply.github.com>
2015-08-23 17:00:41 -0700

    Adding SetInfo and WorkItem to help aggregate set status

---
+++ PyriteCliCommon/Contracts/SetInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PyriteCliCommon.Contracts
+{
+    public class SetInfo
+    {
+        public string Id { get; set; }
+        public string Container { get; set; }
+        public string Path { get; set; }
+        public DateTime QueuedAt { get; set; }
+        public string Status { get; set; }
+        public DateTime? CompletedAt { get; set; }
+        public int ExpectedWorkItems { get; set; }
+        public ICollection<WorkItem> InProgressWorkItems { get; set; }
+        public ICollection<WorkItem> CompletedWorkItems { get; set; }
+    }
+}
---
+++ PyriteCliCommon/Contracts/WorkItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PyriteCliCommon.Contracts
+{
+    public class WorkItem
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public DateTime StartedAt { get; set; }
+        public DateTime? CompletedAt { get; set; }
+    }
+}
